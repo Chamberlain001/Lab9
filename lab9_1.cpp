69188e47c3a5521ac4e8ab3fb118f6ecbb66fe05
Kitpakorn <kitpakorn.chopper@gmail.com>
2022-12-23 16:23:53 +0700

    Edited

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
-  int rank;
+  char rank;
+  int num;
   cout << "Input your rank: ";
   cin >> rank;
-  if(rank == 'S') rank = 'z';
-  else if(rank == 'A') rank = 'y';
-  else if(rank == 'B') rank = 'x';
-  else if(rank == 'C') rank = 'w';
-  else if(rank == 'D') rank = 'v';
-  if(rank == 'z') cout << "You have received Super Ultra Rare Unit!!!\n";
-  if(rank >= 'y') cout << "You have received 5 gems.\n";
-  if(rank >= 'x') cout << "You have received 1 gems.\n";
-  if(rank >= 'w') cout << "You have received 2000 coins.\n";
-  if(rank >= 'v') cout << "You have received very KAK items.\n";
+  if(rank == 'S') num = 5;
+  else if(rank == 'A') num = 4;
+  else if(rank == 'B') num = 3;
+  else if(rank == 'C') num = 2;
+  else if(rank == 'D') num = 1;
+  if(num == 5) cout << "You have received Super Ultra Rare Unit!!!\n";
+  if(num >= 4) cout << "You have received 5 gems.\n";
+  if(num >= 3) cout << "You have received 1 gems.\n";
+  if(num >= 2) cout << "You have received 2000 coins.\n";
+  if(num >= 1) cout << "You have received very KAK items.\n";
   return 0;
 }
